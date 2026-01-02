9d67b8fad42399ab37d51ccbf99e64a8b231baff
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2026-01-01 22:36:34 -0600

    Scatter plot

--- main.py
+++ main.py
@@ -38,10 +38,21 @@ campos = nacional_grado.get_column("campo").unique()
 for campo in campos:
     st.markdown(f"## {campo}")
     nacional_campo = nacional_grado.filter(pl.col("campo") == campo)
-    plot = go.Figure(
-        data=go.Scatter(
+    score_nacional = nacional_campo.filter(pl.col("tipo") == "Nacional")["score"][0]
+    nacional_df = nacional_campo.select(["periodo", "grado_nombre", "campo", "tipo", "score", "ee"])
+    nacional_df.columns = ["Aplicación", "Grado", "Campo", "Tipo", "Puntaje", "Error estándar"]
+    plot = go.Figure()
+    plot.add_hline(
+        y=score_nacional,
+        name="Media nacional",
+        line_color="#7768AE",
+        showlegend=True,
+    )
+    plot.add_trace(
+        go.Scatter(
             x=nacional_campo.get_column("tipo"),
             y=nacional_campo.get_column("score"),
+            name="Puntaje",
             text=nacional_campo.get_column("score").round(2),
             textposition="middle right",
             mode="markers+text",
@@ -52,4 +63,7 @@ for campo in campos:
             ),
         )
     )
+    plot.update_layout(margin=dict(t=30))
+
     st.plotly_chart(plot, key=f"{periodo}{grado}{campo}")
+    st.dataframe(nacional_df.to_pandas().set_index("Aplicación"))
