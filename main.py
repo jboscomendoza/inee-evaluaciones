f2cd76cbffbc4db76dbe586c45a446f658465a5a
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2026-01-04 19:39:21 -0600

    Presentación en columnas

--- main.py
+++ main.py
@@ -3,11 +3,25 @@ import polars as pl
 import plotly.graph_objects as go
 
 COLOR_BARRA = {
-    "Nivel 1": "#E15554",
-    "Nivel 2": "#E1BC29",
-    "Nivel 3": "#3BB273",
-    "Nivel 4": "#7768AE",
+    "Nivel 1": "#f3722c",
+    "Nivel 2": "#f9c74f",
+    "Nivel 3": "#90be6d",
+    "Nivel 4": "#577590",
 }
+COLOR_LINEA = "#b8c0ff"
+COLOR_PUNTO = "#4D9DE0"
+COLS_SCORE = ["periodo", "grado_nombre", "campo", "tipo", "score", "ee"]
+COLS_SCORE_NOMBRE = [
+    "Aplicación",
+    "Grado",
+    "Campo",
+    "Tipo",
+    "Puntaje",
+    "Error estándar",
+]
+COLS_LOGRO = ["periodo", "grado_nombre", "campo", "tipo", "nivel", "porcentaje"]
+COLS_LOGRO_NOMBRE = ["Aplicación", "Grado", "Campo", "Tipo", "Nivel", "Porcentaje"]
+MARGENES = dict(t=30, b=40)
 
 ruta = "data/PLANEA_{m}_nacional.parquet"
 planea_score = pl.read_parquet(ruta.format(m="score"))
@@ -17,54 +31,57 @@ nacional_score = planea_score.filter(pl.col("subpoblacion") == "Nacional")
 nacional_logro = planea_logro.filter(pl.col("subpoblacion") == "Nacional")
 
 ### Streamlit ###
+st.set_page_config(
+    page_title="INEE - Resultados de evaluaciones del aprendizaje",
+    page_icon="✅",
+    layout="wide",
+)
+
 st.title("Resultados nacionales")
 
-col1, col2 = st.columns(2)
+col_select_1, col_select_2, col_select_3 = st.columns(3)
+with col_select_1:
+    periodos = nacional_score.get_column("periodo").unique()
+    periodo = st.selectbox("Año", options=periodos, index=0)
+    score_periodo = nacional_score.filter(pl.col("periodo") == periodo)
+    logro_periodo = nacional_logro.filter(pl.col("periodo") == periodo)
 
-periodos = nacional_score.get_column("periodo").unique()
-with col1:
-    periodo = st.selectbox("Año", options=periodos)
-score_periodo = nacional_score.filter(pl.col("periodo") == periodo)
-logro_periodo = nacional_logro.filter(pl.col("periodo") == periodo)
+with col_select_2:
+    grados = score_periodo.get_column("grado_nombre").unique()
+    grado = st.selectbox("Grado", options=grados, index=0)
+    score_grado = score_periodo.filter(pl.col("grado_nombre") == grado)
+    logro_grado = logro_periodo.filter(pl.col("grado_nombre") == grado)
 
-grados = score_periodo.get_column("grado_nombre").unique()
-with col2:
-    grado = st.selectbox("Asignatura", options=grados)
-score_grado = score_periodo.filter(pl.col("grado_nombre") == grado)
-logro_grado = logro_periodo.filter(pl.col("grado_nombre") == grado)
+with col_select_3:
+    campos = score_grado.get_column("campo").unique()
+    campo = st.selectbox("Campo:", options=campos, index=0)
 
-campos = score_grado.get_column("campo").unique()
+logro_campo = logro_grado.filter(pl.col("campo") == campo)
+score_campo = score_grado.filter(pl.col("campo") == campo)
 
-for campo in campos:
-    st.markdown(f"## {campo}")
-    score_campo = score_grado.filter(pl.col("campo") == campo)
-    logro_campo = logro_grado.filter(pl.col("campo") == campo)
+st.markdown(f"## {campo}")
+
+col_plot_1, col_plot_2 = st.columns(2, gap="large")
+with col_plot_1:
     score_nacional = score_campo.filter(pl.col("tipo") == "Nacional")["score"][0]
-    score_df = score_campo.select(
-        ["periodo", "grado_nombre", "campo", "tipo", "score", "ee"]
-    )
-    score_df.columns = [
-        "Aplicación",
-        "Grado",
-        "Campo",
-        "Tipo",
-        "Puntaje",
-        "Error estándar",
-    ]
-    plot = go.Figure()
-    plot.add_hline(
+
+    st.markdown("### Puntaje")
+
+    score_plot = go.Figure()
+    score_plot.add_hline(
         y=score_nacional,
         name="Media nacional",
-        line_color="#7768AE",
+        line_color=COLOR_LINEA,
         showlegend=True,
     )
-    plot.add_trace(
+    score_plot.add_trace(
         go.Scatter(
             x=score_campo.get_column("tipo"),
             y=score_campo.get_column("score"),
             name="Puntaje",
             text=score_campo.get_column("score").round(2),
             textposition="middle right",
+            marker_color=COLOR_PUNTO,
             mode="markers+text",
             error_y=dict(
                 type="data",
@@ -73,35 +90,37 @@ for campo in campos:
             ),
         )
     )
-    plot.update_layout(margin=dict(t=30))
+    score_plot.update_layout(margin=MARGENES)
 
-    logro_df = logro_campo.select(
-        ["periodo", "grado_nombre", "campo", "tipo", "nivel", "porcentaje"]
-    )
-    logro_df.columns = [
-        "Aplicación",
-        "Grado",
-        "Campo",
-        "Tipo",
-        "Nivel",
-        "Porcentaje",
-    ]
+    score_df = score_campo.select(COLS_SCORE)
+    score_df.columns = COLS_SCORE_NOMBRE
+    st.plotly_chart(score_plot, key=f"{periodo}{grado}{campo}_score")
+    st.dataframe(score_df.to_pandas().set_index("Aplicación"))
 
+with col_plot_2:
+    st.markdown("### Niveles de logro")
+
+    niveles = logro_campo.sort("nivel").get_column("nivel").unique(maintain_order=True)
     plot_logro = go.Figure()
-    for nivel in (
-        logro_campo.sort("nivel").get_column("nivel").unique(maintain_order=True)
-    ):
+    for nivel in niveles:
         logro_nivel = logro_campo.filter(pl.col("nivel") == nivel)
         plot_logro.add_trace(
             go.Bar(
                 name=nivel,
-                x=logro_nivel["tipo"],
-                y=logro_nivel["porcentaje"],
+                y=logro_nivel["tipo"],
+                x=logro_nivel["porcentaje"],
+                text=logro_nivel["porcentaje"],
                 marker=dict(color=COLOR_BARRA[nivel]),
+                orientation="h",
             )
         )
-    plot_logro.update_layout(barmode="stack")
-    st.plotly_chart(plot, key=f"{periodo}{grado}{campo}")
-    st.dataframe(score_df.to_pandas().set_index("Aplicación"))
+    plot_logro.update_layout(barmode="stack", margin=MARGENES)
+
+    logro_df = logro_campo.select(COLS_LOGRO)
+    logro_df.columns = COLS_LOGRO_NOMBRE
+    logro_df = logro_df.pivot(
+        on="Nivel", values="Porcentaje", index=["Aplicación", "Grado", "Campo", "Tipo"]
+    )
+
     st.plotly_chart(plot_logro, key=f"{periodo}{grado}{campo}_logro")
     st.dataframe(logro_df.to_pandas().set_index("Aplicación"))
